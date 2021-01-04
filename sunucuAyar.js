83d4a6a178c5e3dac2bf4a79647c4f2d55e52733
Presty12 <73420849+Presty12@users.noreply.github.com>
2021-01-04 08:08:34 +0000

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: '790861621386936330', // sunucu idsi
+  ownerRole:"790861621743714355", // sunucu sahiplerinin rolü
+  teyitsizRolleri:['790861621479211017'], // kayıt olmayanlara verieln rol
+  jailRolu:"790861621676605445", // cezalılara verilen rol
+  enAltYetkiliRolu:['790861621648031768'], // ilk yetkili permi
+  activity:"S'anta", // botun durumu
+  status:"online",
+  invitelink:"https://discord.gg/NMw3k9QbFs", //davet linki
+  symbol:"✩", // tag
+  voicechannel:"795377106426658836" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
